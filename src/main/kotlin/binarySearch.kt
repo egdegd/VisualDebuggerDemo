25ede907c8848e216a84d3db63e9a5c05be747a7
dmitrii.kirkhmeier <dmitrii.kirkhmeier@jetbrains.com>
2025-03-13 18:54:51 +0100

    broken the regular binarySearch for demo

--- src/main/kotlin/binarySearch.kt
+++ src/main/kotlin/binarySearch.kt
@@ -4,24 +4,18 @@ fun binarySearch(arr: IntArray, target: Int): Int? {
     var mid = right / 2
 
     while (left <= right) {
-        if (arr[mid] == target) {
-            return mid
-        }
-        if (arr[mid] < target) {
-            left = mid + 1
-        }
-        else {
-            right = mid - 1
-        }
+        if (arr[mid] == target) return mid
+        else if (arr[mid] < target) left = mid + 1
+        else right = mid + 1
         mid = left + (right - left) / 2
     }
     return null
 }
 
 fun main() {
-    val sortedArray = intArrayOf(1, 6, 7, 9, 11, 13, 135)
+    val sortedArray = intArrayOf(1, 2, 3, 4, 5, 6)
 
-    val target = 13
+    val target = 4
 
     val result = binarySearch(sortedArray, target)
     if (result != null) {
